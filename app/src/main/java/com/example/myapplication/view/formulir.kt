d10d6b042d1284a6549b6acc5e1534c8adbfb1ff
thekill022 <gibfathoni@gmail.com>
2025-11-09 00:55:43 +0700

    menambahkan button untuk navigasi

--- app/src/main/java/com/example/myapplication/view/formulir.kt
+++ app/src/main/java/com/example/myapplication/view/formulir.kt
@@ -1,12 +1,19 @@
 package com.example.myapplication.view
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.selection.selectable
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ElevatedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
@@ -15,8 +22,10 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.semantics.Role
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
@@ -103,7 +112,7 @@ fun Formulir(modifier : Modifier) {
                 ) {
                     
                     RadioButton(selected = (statuss == selectedStatus), onClick = null)
-                    Text(text = statuss)
+                    Text(text = statuss, modifier = Modifier.padding(end = 10.dp))
                     
                 }
             }
@@ -120,6 +129,36 @@ fun Formulir(modifier : Modifier) {
                 .padding(start = 30.dp, top = 10.dp, end = 30.dp)
                 .fillMaxWidth()
         )
-
+        
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.SpaceEvenly
+        ) {
+            Button(onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .padding(bottom = 25.dp)
+                    .width(120.dp)
+                    .height(50.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.White
+                ),
+                shape = RoundedCornerShape(10.dp),
+                border = BorderStroke(2.dp, Color.Blue)
+            ) {
+                Text(text = "Kembali", color = Color.Blue)
+            }
+            Button(onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .padding(bottom = 25.dp)
+                    .width(120.dp)
+                    .height(50.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.Blue
+                ),) {
+                Text(text = "Submit")
+            }
+        }
+        
     }
 }
