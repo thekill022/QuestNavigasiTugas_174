21114f8f3009b8ca1c98e435d1b7b2a55c32d681
thekill022 <gibfathoni@gmail.com>
2025-11-08 21:57:49 +0700

    membuat composable function SplashScreen

---
+++ app/src/main/java/com/example/myapplication/view/splashscreen.kt
@@ -0,0 +1,9 @@
+package com.example.myapplication.view
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun SplashScreen(modifier : Modifier) {
+}
