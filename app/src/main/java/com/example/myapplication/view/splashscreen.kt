b3bbf987b68aafd1c90475a2e89baf15efb9028d
thekill022 <gibfathoni@gmail.com>
2025-11-08 22:07:02 +0700

    menambahkan image untuk splashscreen

--- app/src/main/java/com/example/myapplication/view/splashscreen.kt
+++ app/src/main/java/com/example/myapplication/view/splashscreen.kt
@@ -1,15 +1,21 @@
 package com.example.myapplication.view
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun SplashScreen(modifier : Modifier) {
     
     Column {
-        Text(text = "Selamat Datang")
+        Text(text = "Selamat Datang", modifier = Modifier.fillMaxWidth().padding(top = 90.dp), fontSize = 26.sp, textAlign = TextAlign.Center)
+
     }
     
 }
