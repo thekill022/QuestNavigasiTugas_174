fa8890f7c792f49a492006e4fb2162f79f7a3be4
thekill022 <gibfathoni@gmail.com>
2025-11-08 23:52:52 +0700

    menambahkan button untuk navigasi ke beranda dan formulir

--- app/src/main/java/com/example/myapplication/view/displaydata.kt
+++ app/src/main/java/com/example/myapplication/view/displaydata.kt
@@ -5,13 +5,21 @@ import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
@@ -73,6 +81,41 @@ fun DisplayData(modifier : Modifier) {
 
                     }
                 }
+            }
+        }
+        
+        Row(
+            modifier = Modifier.fillMaxSize(),
+            verticalAlignment = Alignment.Bottom,
+            horizontalArrangement = Arrangement.Center
+        ) {
+
+            Button(onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .padding(bottom = 30.dp)
+                    .width(130.dp)
+                    .height(60.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.White
+                ),
+                shape = RoundedCornerShape(10.dp),
+                border = BorderStroke(2.dp, Color.Blue)
+            ) {
+                Text(text = "Beranda", color = Color.Blue, fontSize = 20.sp)
+            }
+            Spacer(modifier = Modifier.width(30.dp))
+            Button(onClick = { /*TODO*/ },
+                modifier = Modifier
+                    .padding(bottom = 30.dp)
+                    .width(130.dp)
+                    .height(60.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color.White
+                ),
+                shape = RoundedCornerShape(10.dp),
+                border = BorderStroke(2.dp, Color.Blue)
+            ) {
+                Text(text = "Formulir", color = Color.Blue, fontSize = 20.sp)
             }
         }
 
