0051e9010acfe8cc8bf0b610ff901f4687303abe
thekill022 <gibfathoni@gmail.com>
2025-11-08 22:28:09 +0700

    menambahkan text untuk judul halaman

--- app/src/main/java/com/example/myapplication/view/displaydata.kt
+++ app/src/main/java/com/example/myapplication/view/displaydata.kt
@@ -1,9 +1,17 @@
 package com.example.myapplication.view
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 @Composable
 fun DisplayData(modifier : Modifier) {
 
+    Column {
+        Text(text = "List Daftar Peserta", fontSize = 30.sp)
+    }
+    
 }
