edda0919fbebbeb21315d5baa0587e74aba1c967
thekill022 <gibfathoni@gmail.com>
2025-11-08 21:58:33 +0700

    membuat tata letak column dan text selamat datang

--- app/src/main/java/com/example/myapplication/view/splashscreen.kt
+++ app/src/main/java/com/example/myapplication/view/splashscreen.kt
@@ -1,9 +1,15 @@
 package com.example.myapplication.view
 
 import androidx.compose.foundation.layout.Column
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 
 @Composable
 fun SplashScreen(modifier : Modifier) {
+    
+    Column {
+        Text(text = "Selamat Datang")
+    }
+    
 }
